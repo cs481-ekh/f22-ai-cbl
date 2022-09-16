85b9fe628df2b8a01f2d96903e1648d6cb7b58a5
Nina <nninanikitina@gmail.com>
2022-09-16 12:01:50 -0600

    Task #37

---
+++ knee_stress_predict/config.py
@@ -0,0 +1,7 @@
+# config.py
+
+from pathlib import Path
+
+raw_data_dir = Path(r'D:\University\Classes\CS481_Senior_Design_Project\knee_stress_predict\data\raw')
+processed_data_dir = Path(r'D:\University\Classes\CS481_Senior_Design_Project\knee_stress_predict\data\processed')
+cleaned_data_dir = Path(r'D:\University\Classes\CS481_Senior_Design_Project\knee_stress_predict\data\cleaned')
---
+++ knee_stress_predict/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is purely a research project whose aim is to create and test a machine learning algorithm that can predict knee peak stress/strain with specified accuracy based on the geometry of the knee and loading conditions. ',
+    author='ai-cbl',
+    license='Academic Free License v3.0',
+)
