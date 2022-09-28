79fe821b466655331b47c1e48d54640607afe034
Nina <nninanikitina@gmail.com>
2022-09-27 23:34:27 -0600

    test30

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@ readonly MIN_PASSING_SCORE=0.00
 readonly ERROR_MSG="Aborting commit. Your commit has a pylint score lower than ${MIN_PASSING_SCORE}"
 echo "Starting a script to run pylint on python files.";
 echo ">Running Pylint scan for XXX python package";
-pylint ./knee_stress_predict/**/*.py | grep rated  | sed -n '$s/[^0-9]*\([0-9.]*\).*/\1/p'
+SCORE=$(pylint ./knee_stress_predict/**/*.py | grep rated  | sed -n '$s/[^0-9]*\([0-9.]*\).*/\1/p')
+echo $SCORE
 
 echo "Pylint Run Complete.  Final Status $status"
 exit $status
