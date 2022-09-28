d8e8a6f3b26b65da0b62501fce24193f9b3c5f91
Nina <68862634+ninanikitina@users.noreply.github.com>
2022-09-27 23:45:45 -0600

    60 create testsh (#61)
    * update test.sh
    
    * Update pylint.yml
    
    * test
    
    * test
    
    * test3
    
    * test4
    
    * test5
    
    * test6
    
    * test7
    
    * test8
    
    * test9
    
    * test10
    
    * test11
    
    * test12
    
    * test13
    
    * test14
    
    * test15
    
    * test16
    
    * test17
    
    * test18
    
    * test19
    
    * test20
    
    * test21
    
    * test22
    
    * test23
    
    * test24
    
    * test25
    
    * test26
    
    * test27
    
    * test28
    
    * test29
    
    * test30
    
    * test31

--- test.sh
+++ test.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-exit 0
+readonly MIN_PASSING_SCORE=0.00
+readonly ERROR_MSG="Aborting commit. Your commit has a pylint score lower than ${MIN_PASSING_SCORE}"
+echo "Starting a script to run pylint on python files.";
+echo ">Running Pylint scan for XXX python package";
+SCORE=$(pylint ./knee_stress_predict/**/*.py | grep rated  | sed -n '$s/[^0-9]*\([0-9.]*\).*/\1/p')
+echo $SCORE
+
+if (($(echo "$SCORE < ${MIN_PASSING_SCORE}" | bc -l))); then
+                # and print the error message
+                echo ">$ERROR_MSG" >&2
+                # and save the last_status as failure
+                last_status=1
+            else
+                echo ">Pylint score $SCORE greater than min required score: ${MIN_PASSING_SCORE}; Success!"
+                last_status=0
+            fi
+            # Update the status if any pylint scan for a package fails
+            if [ $last_status -ne 0 ]; then
+                echo "FAILURE $toxfile: [$last_status]"
+                status=$last_status;
+            fi
+
+echo "Pylint Run Complete.  Final Status $status"
+exit $status
