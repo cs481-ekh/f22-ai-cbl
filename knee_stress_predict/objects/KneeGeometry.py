584f15a9bb265b04fb12c9b683f1ad580d1ad7b7
Nina <68862634+ninanikitina@users.noreply.github.com>
2022-10-06 16:40:44 -0600

    67 prepare point cloud data for linear regression analysis (#87)
    * #71
    
    * #70
    
    * #85
    
    * #72

--- knee_stress_predict/objects/KneeGeometry.py
+++ knee_stress_predict/objects/KneeGeometry.py
@@ -1,8 +1,16 @@
 from pathlib import Path
 import pyvista as pv
+from scipy.spatial import KDTree
 from knee_stress_predict.config import raw_data_dir
 import re
+import numpy as np
+from pyvista import examples
 
+def generate_points(subset=0.02):
+    """A helper to make a 3D NumPy array of points (n_points by 3)"""
+    dataset = examples.download_lidar()
+    ids = np.random.randint(low=0, high=dataset.n_points - 1, size=int(dataset.n_points * subset))
+    return dataset.points[ids]
 
 class KneeGeometry(object):
     def __init__(self, path):
@@ -41,7 +49,26 @@ class KneeGeometry(object):
 if __name__ == '__main__':
     path = Path.joinpath(raw_data_dir, "set_1/2022.09.16_Geometries")
     knee = KneeGeometry(path)
+    knee.femur.plot(style='wireframe', color='tan')
     merged = knee.femur.merge(knee.tibia_cart_med)
-    # merged = merged.merge(knee.tibia_cart_med)
     merged.plot(style='wireframe', color='tan')
-    # knee.femur.plot(jupyter_backend="static")
+
+    # Testing distance extraction
+    surf_femur = knee.femur.extract_surface()
+    _ = knee.tibia_cart_med.compute_implicit_distance(surf_femur, inplace=True)
+    _ = knee.tibia_cart_lat.compute_implicit_distance(surf_femur, inplace=True)
+
+    pl = pv.Plotter()
+    _ = pl.add_mesh(knee.tibia_cart_med, scalars='implicit_distance', cmap='bwr')
+    _ = pl.add_mesh(knee.tibia_cart_lat, scalars='implicit_distance', cmap='bwr')
+    _ = pl.add_mesh(surf_femur, color='w', style='wireframe')
+    pl.show()
+
+    implicit_distance = knee.tibia_cart_med.active_scalars
+    min = min(implicit_distance)
+    max = max(implicit_distance)
+    mean = np.mean(implicit_distance)
+    print(min, max, mean)
+
+
+
